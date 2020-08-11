e7a5c4b9dcea62aa2cc4978a7e2517aa15ca4aae
Daniel <jdb12@rice.edu>
2020-08-11 11:53:42 -0400

    mount the entire star-sw instead of only StRoot Needed for the recent renames (StgMaker-->StFwdTrackMaker) build to pass

--- dev.sh
+++ dev.sh
@@ -8,5 +8,9 @@ set -euo pipefail
 # it mounts the subdirectories of this project into the container for inter op
 #
 
-echo docker run --rm -ti $@ -w /tmp/work -v `pwd`/work:/tmp/work -v `pwd`/star-sw/StRoot:/tmp/star-sw-master/StRoot jdbburg/star-sw-buil:v041820d bash
-docker run --rm -ti $@ -w /tmp/work -v `pwd`/work:/tmp/work -v `pwd`/star-sw/StRoot:/tmp/star-sw-master/StRoot jdbburg/star-sw-build:v041820 bash 
+# echo docker run --rm -ti $@ -w /tmp/work -v `pwd`/work:/tmp/work -v `pwd`/star-sw/StRoot:/tmp/star-sw-master/StRoot jdbburg/star-sw-buil:v041820d bash
+# docker run --rm -ti $@ -w /tmp/work -v `pwd`/work:/tmp/work -v `pwd`/star-sw/StRoot:/tmp/star-sw-master/StRoot jdbburg/star-sw-build:v041820 bash 
+
+
+echo docker run --rm -ti $@ -w /tmp/work -v `pwd`/work:/tmp/work -v `pwd`/star-sw:/tmp/star-sw-master jdbburg/star-sw-buil:v041820d bash
+docker run --rm -ti $@ -w /tmp/work -v `pwd`/work:/tmp/work -v `pwd`/star-sw:/tmp/star-sw-master jdbburg/star-sw-build:v041820 bash 
